0d72305c2daf247917250f7e018679ba0e078551
ifndev <waterfoxyt@gmail.com>
2020-05-16 13:32:44 +0200

    ✨ Minimum logging level

--- flogs.v
+++ flogs.v
@@ -2,15 +2,10 @@
 USAGE:
 
 log(<String>, < .verbose | .info | .warning | .error | .critical >)
-
 */
-
 module flogs
-import term
 
-const (
-	colors_supported = term.can_show_color_on_stdout()
-)
+import term
 
 pub enum Level {
 	verbose
@@ -20,21 +15,44 @@ pub enum Level {
 	critical
 }
 
-pub fn log(txt string, lvl Level) { //NOTE: Consider making the Level optional, defaulting to verbose or info
-	match colors_supported {  // Dirty !
-		true { println(match lvl {
-				.verbose { '\033[90m[VERBOSE] ' + txt + '\033[0m' }
-				.info { '[\033[34mINFO\033[0m] ' + txt + '\033[0m' }
-				.warning { '[\033[33mWARNING\033[0m] ' + txt + '\033[0m' }
-				.error { '[\033[31mERROR\033[0m] ' + txt + '\033[0m' }
-				.critical { '\033[1;31m[CRITICAL] ' + txt + '\033[0m' }
-			}) }
-		else { println(match lvl {
-				.verbose { '[VERBOSE] ' + txt }
-				.info { '[INFO] ' + txt}
-				.warning { '[WARNING] ' + txt}
-				.error { '[ERROR] ' + txt}
-				.critical { '[CRITICAL] ' + txt}
-			}) }
+const (
+	colors_supported = term.can_show_color_on_stdout()
+	levels_ranking   = [flogs.Level.critical, .error, .warning, .info, .verbose]
+)
+
+pub struct Logger {
+	min_logging_level Level
+}
+
+fn is_important_enough(lvl, minlvl Level) bool {
+	for value in levels_ranking {
+		match value {
+			lvl {return true}
+			minlvl {return false}
+			else {}
+		}
+	}
+	return false
+}
+
+pub fn (l Logger) log(txt string, lvl Level) { // NOTE: Consider making the Level optional, defaulting to verbose or info
+	match is_important_enough(lvl, l.min_logging_level) {
+		true { match colors_supported {
+				true { println(match lvl {
+						.verbose { '\033[90m[VERBOSE] ' + txt + '\033[0m' }
+						.info { '[\033[34mINFO\033[0m] ' + txt + '\033[0m' }
+						.warning { '[\033[33mWARNING\033[0m] ' + txt + '\033[0m' }
+						.error { '[\033[31mERROR\033[0m] ' + txt + '\033[0m' }
+						.critical { '\033[1;31m[CRITICAL] ' + txt + '\033[0m' }
+					}) }
+				else { println(match lvl {
+						.verbose { '[VERBOSE] ' + txt }
+						.info { '[INFO] ' + txt }
+						.warning { '[WARNING] ' + txt }
+						.error { '[ERROR] ' + txt }
+						.critical { '[CRITICAL] ' + txt }
+					}) }
+			} }
+		else {}
 	}
 }
